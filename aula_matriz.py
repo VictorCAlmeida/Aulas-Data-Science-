d72378ba996c747528346cb4c9d443c60b3e2737
VictorCAlmeida <resultadoscomprojetos@gmail.com>
2026-03-01 19:44:25 -0300

    Meu primeiro commit

---
+++ aula_matriz.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr2 =np.array([
+    [22,45,678,345,3],
+    [541,878,112,48,11],
+    [754,54,84,221,25],
+    [21,84,845,611,41]
+])
+
+print (arr2[2,3])
+
+#Informações da matriz
+
+print(f"Shape da matriz: {arr2.shape}")
+print(f"Número de elementos: {arr2.size}")
+print (f"Tipo dos elementos: {arr2.dtype}")
